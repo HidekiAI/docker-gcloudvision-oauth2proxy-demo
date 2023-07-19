608f2445225f234b4c1d271867f17ff3ff9158a6
Hideki A. Ikeda <hidekiai+hidekiai-lt@CodeMonkeyNinja.dev@users.noreply.github.com>
2023-07-19 16:22:12 -0500

    More adjustments to make script work - grabbing lastest linux-amd64 version of oauth2-proxy from github - fixed issues of Dockerfile looking for src and Cargo.toml from wrong   directory

--- build_image.sh
+++ build_image.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
-
 pushd . 2>&1 > /dev/null
-cd Oath2-Proxy/
+cd Oauth2-Proxy/
 docker build -t oauth2-proxy-image .
 popd
-
 
 pushd . 2>&1 > /dev/null
 cd GCloudVision/
--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+
+if ! [ -e docker-compose.yml ] ; then
+	echo "# ERROR: Unable to locate 'docker-compose.yml' file in the current directory '$(pwd)'"
+	exit -1
+fi
+
+# stop images if running
+docker-compose down
+
+# first, build images
+./build_image.sh
+
+# now run
 docker-compose up -d
