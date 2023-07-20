bf0c0b1dff3f1fdcc48b0a2c58c17b311bd5dcce
Hideki A. Ikeda <hidekiai+hidekiai-lt@CodeMonkeyNinja.dev@users.noreply.github.com>
2023-07-20 12:31:19 -0500

    Removed Oauth2 git pull from github and instead, using official docker image

--- build_image.sh
+++ build_image.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-pushd . 2>&1 > /dev/null
-cd Oauth2-Proxy/
-docker build -t oauth2-proxy-image .
-popd
+_DOCKER=$(which docker)
+if [ x"${_DOCKER}" == x"" ]; then
+	echo "# ERROR: Unable to locate docker"
+	exit -1
+fi
+
+# According to https://oauth2-proxy.github.io/oauth2-proxy/docs/, there is already a docker image, so we'll use the official image
+docker pull quay.io/oauth2-proxy/oauth2-proxy:latest
 
 pushd . 2>&1 > /dev/null
 cd GCloudVision/
 docker build -t my-rust-app-image .
 popd
+
+docker image ls
